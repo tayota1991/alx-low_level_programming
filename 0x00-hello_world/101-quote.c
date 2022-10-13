513413df2e8c1c77d9d6ed0561da49e7fc924ccd
tayota <tayota1991@gmail.com>
2022-10-13 08:08:08 -0700

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print to STDERR'
+ *
+ * Return: Always 1 (Success)
+*/
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+        return (1);
+}
+
