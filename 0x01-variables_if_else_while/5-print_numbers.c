395be7cb18733a7be85912b654f608478800bccd
tayota <tayota1991@gmail.com>
2022-10-14 14:32:02 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'putchar function'
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
