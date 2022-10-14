496265e6a8bfb36f547ddcf525b2c0d6b1f329db
tayota <tayota1991@gmail.com>
2022-10-14 09:03:13 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'puts function'
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+{
+	putchar(x);
+}
+	putchar('\n');
+
+	return (0);
+}
+
