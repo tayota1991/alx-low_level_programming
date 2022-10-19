ea2a414b15e9fdcd4b3a6c0e4ae9832cbe241d50
tayota <tayota1991@gmail.com>
2022-10-19 09:31:53 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Check description
+ * Description: computes the absolute value of an integer.
+ * @c: input value
+ * Return: the absolute value
+*/
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (c * -1);
+}
