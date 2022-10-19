2d8eb477422093f63b1c2ecbecfca4c7ca613d0d
tayota <tayota1991@gmail.com>
2022-10-19 13:16:27 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Check description
+ * Description: adds two integers
+ * @x: input value
+ * @y: input value
+ * Return: the result
+*/
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * times_table - Check
+ * Description: prints the 9 times table
+ * Return: Nothing.
+*/
+void times_table(void)
+{
+}
